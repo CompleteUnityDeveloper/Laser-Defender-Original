734038487e60ddf1b2418a30acba62d7813eb530
Brice Fernandes <brice.fernandes@gmail.com>
2014-11-19 12:01:52 +0000

    02 - Importing a menu system

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Start () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
+	}
+}
