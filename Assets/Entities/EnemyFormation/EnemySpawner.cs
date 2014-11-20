212ca4f76d5be4e2e6095db7532234ca18e2768b
Brice Fernandes <brice.fernandes@gmail.com>
2014-11-20 22:33:35 +0000

    05 - Creating Enemy Positions

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -6,8 +6,10 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
-		enemy.transform.parent = transform;
+		foreach( Transform child in transform){
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Entities/EnemyFormation/Position.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
