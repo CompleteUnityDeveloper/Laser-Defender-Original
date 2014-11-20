72a4594c2bead31a11a678bb00b5a4bb084c8ece
Brice Fernandes <brice.fernandes@gmail.com>
2014-11-20 02:38:49 +0000

    04 - Creating the enemies

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 15.0f;
+	public float padding = 1;
+
+	private float xmax = -5;
+	private float xmin = 5;
+	
+	void Start(){
+		Camera camera = Camera.main;
+		float distance = transform.position.z - camera.transform.position.z;
+		xmin = camera.ViewportToWorldPoint(new Vector3(0,0,distance)).x + padding;
+		xmax = camera.ViewportToWorldPoint(new Vector3(1,1,distance)).x - padding;
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x - speed * Time.deltaTime, xmin, xmax),
+				transform.position.y, 
+				transform.position.z 
+			);
+		}else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x + speed * Time.deltaTime, xmin, xmax),
+				transform.position.y, 
+				transform.position.z 
+			);
+		}
+	}
+}
