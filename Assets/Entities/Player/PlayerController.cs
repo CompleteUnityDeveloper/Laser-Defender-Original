a96ab17539067217e83e05fd40f74c5c7bd89a73
Brice Fernandes <brice.fernandes@gmail.com>
2014-12-02 20:47:23 +0000

    07 - Spawning projectiles

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	public GameObject laser;
+	public float projectileSpeed = 10;
+	public float projectileRepeatRate = 0.2f;
+	
 	public float speed = 15.0f;
 	public float padding = 1;
 
@@ -14,8 +18,18 @@ public class PlayerController : MonoBehaviour {
 		xmin = camera.ViewportToWorldPoint(new Vector3(0,0,distance)).x + padding;
 		xmax = camera.ViewportToWorldPoint(new Vector3(1,1,distance)).x - padding;
 	}
-	
+
+	void Fire(){
+		GameObject beam = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed, 0);
+	}
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			InvokeRepeating("Fire", 0.0001f, projectileRepeatRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke("Fire");
+		}
 		if(Input.GetKey(KeyCode.LeftArrow)){
 			transform.position = new Vector3(
 				Mathf.Clamp(transform.position.x - speed * Time.deltaTime, xmin, xmax),
---
+++ Assets/Shredder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		Destroy(col.gameObject);
+	}
+}
