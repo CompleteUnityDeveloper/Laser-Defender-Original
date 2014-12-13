ca45c81d4e3bdf184e2d77bfdbef9af40c864cab
Brice Fernandes <brice.fernandes@gmail.com>
2014-12-13 14:05:54 +0000

    08 - Shooting Enemies

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public float health = 150;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage = 100f;
+	
+	public float GetDamage(){
+		return damage;
+	}
+	
+	public void Hit(){
+		Destroy(gameObject);
+	}
+}
