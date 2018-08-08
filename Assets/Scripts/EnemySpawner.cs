27ead03886ec1fe3e660a466fb7229bae80c779d
Rick <rickinaction@gmail.com>
2018-08-08 15:32:42 +1000

    Somewhat ugly process of spawning and following path.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float spawnDelaySeconds = .5f;
+    [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Transform startingWaypoint;
+
+    // Use this for initialization
+    void Start () {
+        SpawnEnemy();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject newEnemy = Instantiate(enemyPrefab, startingWaypoint.transform.position, Quaternion.identity) as GameObject;
+        Invoke("SpawnEnemy", spawnDelaySeconds);
+    }
+
+}
