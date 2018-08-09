6d328524fb775a0d06913996ac78efb9396c06f5
Ben Tristem <github@bentristem.com>
2018-08-09 12:57:18 +0100

    Rick to test hard crash

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] List<EnemyWave> enemyWaves;
+    [SerializeField] List<EnemyWave> enemyWaves; // start with EnemyWave[] 
     [SerializeField] int startingWave = 0;
     [SerializeField] bool looping = false;
 
@@ -13,23 +13,28 @@ public class EnemySpawner : MonoBehaviour
         do
         {
             StartCoroutine(SpawnAllWaves());
+            print("Spawing all waves");
         }
         while (looping);
     }
 
-    IEnumerator SpawnAllWaves()
+    private IEnumerator SpawnAllWaves()
     {
+        // start enemyWaves.Length, then change to list with enemyWaves.Count
         for (int waveIndex = startingWave; waveIndex < enemyWaves.Count; waveIndex++)
         {
-            yield return StartCoroutine(SpawnAllEnemiesInWave(enemyWaves[waveIndex]));
-            // "yield return" so we wait before moving on. Series co-routine
+            print("Spawing wave " + waveIndex);
+            var currentWave = enemyWaves[waveIndex]; // note this isn't null-safe
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+            // Series co-routine: "yield return" so we wait before moving on.
         }
     }
 
-    IEnumerator SpawnAllEnemiesInWave(EnemyWave wave)
+    private IEnumerator SpawnAllEnemiesInWave(EnemyWave wave)
     {
         for (int enemyCount = 0; enemyCount < wave.GetNumberOfEnemies(); enemyCount++)
         {
+            print("Spawning enemy in wave");
            Instantiate(
                 wave.GetEnemyPrefab(),
                 wave.GetStartingWayPoint().transform.position,
