414618a425b9b638f3e7b7abe56c66dbd9344c98
Rick <rickinaction@gmail.com>
2018-08-10 11:26:29 +1000

    Added spiffy scrolling star field and exposion particle effect.

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    [SerializeField] float backgroundScrollSpeed = 1f;
+
+    Material myMaterial;
+    Vector2 offSet;
+
+
+    private void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+    private void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
